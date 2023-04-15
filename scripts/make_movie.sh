a658a7325c0504c6f301a9bfc37cdf3026ed9f3f
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2023-04-15 14:59:17 +0800

    corrections

--- scripts/make_movie.sh
+++ scripts/make_movie.sh
@@ -48,7 +48,7 @@ do
    if [[ $(($i % $movie_png_rate)) == 0 ]]; then
       out_i=$(($i / $movie_png_rate))
       i_str=`echo $out_i | awk '{printf("%06d\n",$1);}'`;    
-      ln -s ${png} image_${i_str}.png; 
+      ln -s ${png} image_${i_str}.${image_type}; 
    fi
    i=$(($i+1));
 done      
@@ -57,8 +57,8 @@ done
 #      mencoder mf://*.png -mf w=800:h=600:fps=10:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o 20200226_eda2.avi      
 echo "rm -f sky_last_24h.mp4"
 rm -f sky_last_24h.mp4
-echo "ffmpeg -framerate 25 -i chan_${freq_ch}_${stokes}_%6d.png -c:v libx264 -vf \"drawtext=fontfile=/usr/share/fonts/truetype/freefont/FreeSans.ttf:text=%{n}:x=(w-tw)/2:y=h-(2*lh): fontcolor=white:box=1:boxcolor=0x00000099\" -pix_fmt yuv420p sky_last_24h.mp4"
-ffmpeg -framerate 25 -i chan_${freq_ch}_${stokes}_%6d.png -c:v libx264 -vf "drawtext=fontfile=/usr/share/fonts/truetype/freefont/FreeSans.ttf:text=%{n}:x=(w-tw)/2:y=h-(2*lh): fontcolor=white:box=1:boxcolor=0x00000099" -pix_fmt yuv420p sky_last_24h.mp4
+echo "ffmpeg -framerate 25 -i image_%6d.${image_type} -c:v libx264 -vf \"drawtext=fontfile=/usr/share/fonts/truetype/freefont/FreeSans.ttf:text=%{n}:x=(w-tw)/2:y=h-(2*lh): fontcolor=white:box=1:boxcolor=0x00000099\" -pix_fmt yuv420p sky_last_24h.mp4"
+ffmpeg -framerate 25 -i image_%6d.${image_type} -c:v libx264 -vf "drawtext=fontfile=/usr/share/fonts/truetype/freefont/FreeSans.ttf:text=%{n}:x=(w-tw)/2:y=h-(2*lh): fontcolor=white:box=1:boxcolor=0x00000099" -pix_fmt yuv420p sky_last_24h.mp4
 
 # if [[ $publish -gt 0 ]]; then
 #   echo "scp sky_last_24h.mp4 aavs1-server:/exports/eda/${station_name}/tv/"
